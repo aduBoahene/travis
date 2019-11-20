fc776ffeaa554131381277eb9cd7d69a956106a2
Patrick Boahene <patrick@acsghana.com>
2019-11-20 17:42:07 +0000

    travis test

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('ci with travis');
+app.get("/", (req, res) => {
+  res.send("ci with travis and node js");
 });
 
 const server = app.listen(3000, () => {
-  console.log('App running on port 3000');
+  console.log("App running on port 3000");
 });
 
 module.exports = server;
